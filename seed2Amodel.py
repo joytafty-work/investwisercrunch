5edb651afee4d58414d149fa5e1cfa07d43e2895
joytafty <joytafty@gmail.com>
2014-03-09 11:38:05 -0700

    update create table

--- seed2Amodel.py
+++ seed2Amodel.py
@@ -12,10 +12,11 @@ def loadfeatures():
 	con = mdb.connect(dbhost, dbusr, dbpwd, dbname)
 
 	with con: 
-    	d = pd.io.sql.read_frame('SELECT DISTINCT * FROM founder_seed2A_funds ORDER BY name', con)     
-    	ppl = pd.io.sql.read_frame('SELECT DISTINCT * FROM founder_seed2A_info ORDER BY company_name', con)
+		d = pd.io.sql.read_frame('SELECT DISTINCT * FROM founder_seed2A_funds ORDER BY name', con)     
+		ppl = pd.io.sql.read_frame('SELECT DISTINCT * FROM founder_seed2A_info ORDER BY company_name', con)
+	
 	con.close()
-
+	
 	return d, ppl
 
 def process_features(d, ppl):
